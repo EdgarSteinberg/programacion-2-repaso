80e1b18d57833fb7503275c2c20e1395dee57f07
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-09-21 13:24:16 -0300

    diagrama de clases

--- clase_16/obj/Debug/net10.0/clase_16.AssemblyInfo.cs
+++ clase_16/obj/Debug/net10.0/clase_16.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clase_16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a691467aaa2fedf69039bdc64a65da6a48071537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c629ca8083f3740bf737a736bb41b386ffb6581f")]
 [assembly: System.Reflection.AssemblyProductAttribute("clase_16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clase_16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
